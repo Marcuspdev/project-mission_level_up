8b6368efb7326c289871fe671966a38f69349496
Marcuspdev <paulo.marcus@escolar.ifrn.edu.br>
2026-09-11 23:17:11 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const avatar = document.querySelector('#avatar');
+
+const tela1 = document.querySelector('#tela1');
+
+const tela2 = document.querySelector('#tela2');
+
+const tela3 = document.querySelector('#tela3');
+
+/* Escolha do avatar */
+
+const sonic = document.querySelector('.sonic');
+
+sonic.addEventListener('click', function() {
+    avatar.src = "img/sonic.png";
+
+    tela1.style.display = "none";
+
+    tela2.style.display = "block";
+
+    console.log("Avatar: sonic");
+});
+
+
+const superman = document.querySelector('.superman');
+
+superman.addEventListener('click', function() {
+    avatar.src = "img/superman.png";
+
+    tela1.style.display = "none";
+
+    tela2.style.display = "block";
+
+    console.log("Avatar: superman");
+});
+
+
+const chapolin = document.querySelector('.chapolin');
+
+chapolin.addEventListener('click', function() {
+    avatar.src = "img/chapolin.png";
+
+    tela1.style.display = "none";
+
+    tela2.style.display = "block";
+
+    console.log("Avatar: chapolin");
+});
+
+/* Escolha de mapa */
+
+const mapa_normal = document.querySelector('#mapa_normal');
+
+mapa_normal.addEventListener('click', function() {
+    tela3.style.backgroundColor = "cyan";
+
+    tela2.style.display = "none";
+
+    tela3.style.display = "block";
+});
+
+
+const mapa_invertido = document.querySelector('#mapa_invertido');
+
+mapa_invertido.addEventListener('click', function() {
+    tela3.style.backgroundColor = "blueviolet";
+
+    tela2.style.display = "none";
+
+    tela3.style.display = "block";
+});
+
+/* Botão de voltar */
+
+const voltar = document.querySelector('#voltar');
+
+voltar.addEventListener('click', function() {
+    tela1.style.display = "block";
+
+    tela3.style.display = "none";
+});
