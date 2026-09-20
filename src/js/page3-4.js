3778388b791e8fb0ed9d4e5e506f45d7f9cad6c4
Marcuspdev <paulo.marcus@escolar.ifrn.edu.br>
2026-09-20 15:42:39 -0300

    feat: add script on page choice avatr and choise world

---
+++ src/js/page3-4.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const secaoAvatar = document.querySelector('#choiceavatar');
+const secaoMundo = document.querySelector('#choiceword');
+
+const dustin = document.querySelector('#dustin');
+const eddie = document.querySelector('#eddie');
+const gorgon = document.querySelector('#gorgon');
+
+let avatar = '';
+let mundo = '';
+
+function avancarParaMundos() {
+    if (secaoAvatar && secaoMundo) {
+        secaoAvatar.classList.add('escondido');
+        secaoMundo.classList.remove('escondido');
+    }
+}
+
+// Quando o avatar for escolhido
+
+if(dustin) {
+    dustin.addEventListener('click', function() {
+        avatar = "dustin";
+        avancarParaMundos()
+})};
+
+if(eddie) {
+    eddie.addEventListener('click', function() {
+        avatar = "eddie";
+        avancarParaMundos()
+})};
+
+if(gorgon) {
+    gorgon.addEventListener('click', function() {
+        avatar = "gorgon";
+        avancarParaMundos()
+})};
+
+// Quando o mundo for escolhido
+
+const mundoNormal = document.querySelector('#mundo-normal');
+
+const mundoInvertido = document.querySelector('#mundo-invertido');
+
+function avancarParaMapa() {
+    window.location.href = "pages/mapa.html?avatar=" + avatar + "&mundo=" + mundo;
+}
+
+if(mundoNormal) {
+    mundoNormal.addEventListener('click', function() {
+        mundo = "normal";
+        avancarParaMapa();
+    });
+}
+
+if(mundoInvertido) {
+    mundoInvertido.addEventListener('click', function() {
+        mundo = "invertido";
+        avancarParaMapa()
+    });
+}
