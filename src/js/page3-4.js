eba5e9810df9a96831e76dd7b9501d7547ef7f07
Marcuspdev <paulo.marcus@escolar.ifrn.edu.br>
2026-09-25 23:15:52 -0300

    fix: revert to previous working commit and fix layout

--- src/js/page3-4.js
+++ src/js/page3-4.js
@@ -41,15 +41,23 @@ if(gorgon) {
 // Quando o mundo for escolhido
 
 const mundoNormal = document.querySelector('#mundo-normal');
-const mundoInvertido = document.querySelector('#mundo-invertido');
-const audioMundoInvertido = new Audio('src/assets/audios/mundoinvertidoaudio.mp3');
-
+const audioMundoNormal = new Audio('src/assets/audios/mundonormalaudio.mp3');
 
 if(mundoNormal) {
     mundoNormal.addEventListener('click', function() {
         window.location.href = "src/pages/mundo-normal.html?avatar=" + avatar + "&nivel=" + nivel;
+
+    });
+    mundoNormal.addEventListener('mouseenter', function() {
+        audioMundoNormal.play();
+    });
+    mundoNormal.addEventListener('mouseleave', function() {
+        audioMundoNormal.pause();
     });
 };
+
+const mundoInvertido = document.querySelector('#mundo-invertido');
+const audioMundoInvertido = new Audio('src/assets/audios/mundoinvertidoaudio.mp3');
 
 if(mundoInvertido) {
     mundoInvertido.addEventListener('click', function() {
