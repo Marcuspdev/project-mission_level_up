34824d21d9be153de1710776be17c75c3a530d78
Marcuspdev <paulo.marcus@escolar.ifrn.edu.br>
2026-09-26 01:38:49 -0300

    fix: adjust layout

--- src/js/page3-4.js
+++ src/js/page3-4.js
@@ -54,12 +54,6 @@ if(mundoNormal) {
     mundoNormal.addEventListener('mouseleave', function() {
         audioMundoNormal.pause();
     });
-    mundoNormal.addEventListener('mouseenter', function() {
-        audioMundoNormal.play();
-    });
-    mundoNormal.addEventListener('mouseleave', function() {
-        audioMundoNormal.pause();
-    });
 };
 
 const mundoInvertido = document.querySelector('#mundo-invertido');
